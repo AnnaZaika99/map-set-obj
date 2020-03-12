a6d0cadaa01bf1dcacc72168f056ba0ca49d53bc
AnnaZaika99 <54880267+AnnaZaika99@users.noreply.github.com>
2020-03-12 22:17:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var map_a = new Map();
+map_a.set('vir1','tov1');
+map_a.set('vir2','tov1');
+map_a.set('vir3','tov2');
+map_a.set('vir4','tov2');
+map_a.set('vir5','tov3');
+
+var map_b = new Map();
+map_a.set(10,'vir1');
+map_a.set(101,'vir2');
+map_a.set(111,'vir2');
+map_a.set(121,'vir3');
+map_a.set(120,'vir3');
+
+var map_c = new Map();
+map_a.set(1,'vir1');
+map_a.set(7,'vir1');
+map_a.set(3,'vir2');
+map_a.set(5,'vir3');
+map_a.set(2,'vir3');
